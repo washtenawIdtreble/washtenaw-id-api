a307b47974f08dffc06614a631c631928ad5872a
Ryan Heisler <info@ryanheisler.com>
2022-03-02 23:13:57 -0500

    RHEI - Add cors to allow cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 export const app: Application = express();
 
+app.use(cors());
 app.use(router);
