c301c16b5eda73e435ea969974d0ae7c028aa62c
Ryan Heisler <info@ryanheisler.com>
2022-03-23 21:50:13 -0400

    RHEI - Add endpoint to get all businesses with their categories

---
+++ src/controllers/all-businesses.test.ts
@@ -0,0 +1,54 @@
+import request from "supertest";
+import { app } from "../app";
+import { stubCategorizedBusinesses } from "../../test-helpers/test-factories";
+
+describe("all businesses endpoint", () => {
+    test("should return a list of categories and businesses", async () => {
+        const response = await request(app).get("/businesses");
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual([
+            stubCategorizedBusinesses({
+                category: "banks", businesses: [
+                    "business 1",
+                    "business 2",
+                    "business 3",
+                ],
+            }),
+            stubCategorizedBusinesses({
+                category: "food", businesses: [
+                    "business 1",
+                    "business 2",
+                    "business 3",
+                ],
+            }),
+            stubCategorizedBusinesses({
+                category: "jobs", businesses: [
+                    "business 1",
+                    "business 2",
+                    "business 3",
+                ],
+            }),
+            stubCategorizedBusinesses({
+                category: "mental health", businesses: [
+                    "business 1",
+                    "business 2",
+                    "business 3",
+                ],
+            }),
+            stubCategorizedBusinesses({
+                category: "pharmacies", businesses: [
+                    "business 1",
+                    "business 2",
+                    "business 3",
+                ],
+            }),
+            stubCategorizedBusinesses({
+                category: "transportation", businesses: [
+                    "business 1",
+                    "business 2",
+                    "business 3",
+                ],
+            }),
+        ]);
+    });
+});
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { handlePing } from "./controllers/ping";
 import { handleCategoriesGet } from "./controllers/categories";
+import { handleBusinessesGet } from "./controllers/all-businesses";
 
 export const router = Router();
 
 router.get("/ping", handlePing);
 router.get("/categories", handleCategoriesGet);
+router.get("/businesses", handleBusinessesGet);
