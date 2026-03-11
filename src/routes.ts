907105723d34bbeaa7826951a9f4e9c066834cb0
Ryan Heisler <info@ryanheisler.com>
2026-03-10 20:20:46 -0400

    Remove unused businesses and categories. We're not using them and we aren't planning to

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import { handlePing } from "./controllers/ping";
-import { handleCategoriesGet } from "./controllers/categories";
-import { handleBusinessesGet } from "./controllers/all-businesses";
 import { handleAccessibilityFormSubmit } from "./controllers/accessibility-form-submit";
 import { handleContactFormSubmit } from "./controllers/contact-form-submit";
 import { handleIdRefusedFormSubmit } from "./controllers/id-refused-form-submit";
@@ -9,8 +7,6 @@ import { handleIdRefusedFormSubmit } from "./controllers/id-refused-form-submit"
 export const router = Router();
 
 router.get("/ping", handlePing);
-router.get("/categories", handleCategoriesGet);
-router.get("/businesses", handleBusinessesGet);
 router.post("/accessibility-issues", handleAccessibilityFormSubmit);
 router.post("/id-refused", handleIdRefusedFormSubmit);
 router.post("/contact-us", handleContactFormSubmit);
