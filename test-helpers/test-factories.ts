530496639db591403bcc0fdbc239d12bb6ef5012
Ryan Heisler <info@ryanheisler.com>
2026-03-10 20:20:46 -0400

    Remove unused businesses and categories. We're not using them and we aren't planning to

--- test-helpers/test-factories.ts
+++ test-helpers/test-factories.ts
@@ -1,38 +1,7 @@
-import { Category } from "../src/controllers/categories";
-import { buildRandomBusiness, Business, CategorizedBusinesses } from "../src/controllers/all-businesses";
 import { AccessibilityFormData } from "../src/controllers/accessibility-form-submit";
 import { ContactFormData } from "../src/controllers/contact-form-submit";
 import { IdRefusedFormData } from "../src/controllers/id-refused-form-submit";
 import { faker } from "@faker-js/faker";
-
-export const stubCategory = (attributes: Partial<Category> = {}): Category => {
-    return {
-        displayName: attributes.displayName === undefined ? "Stub DisplayName" : attributes.displayName,
-        name: attributes.name === undefined ? "stub-name" : attributes.name,
-    };
-};
-
-export const stubBusiness = (attributes: Partial<Business> = {}): Business => {
-    const randomBusiness = buildRandomBusiness();
-
-    return {
-        name: attributes.name === undefined ? randomBusiness.name : attributes.name,
-        address: attributes.address === undefined ? randomBusiness.address : attributes.address,
-        city: attributes.city === undefined ? randomBusiness.city : attributes.city,
-        state: attributes.state === undefined ? randomBusiness.state : attributes.state,
-        zip: attributes.zip === undefined ? randomBusiness.zip : attributes.zip,
-        website: attributes.website === undefined ? randomBusiness.website : attributes.website,
-        phone: attributes.phone === undefined ? randomBusiness.phone : attributes.phone,
-        description: attributes.description === undefined ? randomBusiness.description : attributes.description,
-    };
-};
-
-export const stubCategorizedBusinesses = (attributes: Partial<CategorizedBusinesses> = {}): CategorizedBusinesses => {
-    return {
-        category: attributes.category === undefined ? stubCategory() : attributes.category,
-        businesses: attributes.businesses === undefined ? [stubBusiness({ name: "Stub Business" })] : attributes.businesses,
-    };
-};
 
 export const stubIdRefusedFormData = (attributes: Partial<IdRefusedFormData> = {}): IdRefusedFormData => {
     return {
